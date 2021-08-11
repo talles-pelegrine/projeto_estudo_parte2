7d4feb90643324b696ecd7885fda53c8229a1f21
tspu <talles.souza@gft.com>
2021-08-11 11:39:35 -0300

    Initial commit

---
+++ src/main/java/com/study/parttwo/StudyPartTwo/StudyPartTwoApplication.java
@@ -0,0 +1,13 @@
+package com.study.parttwo.StudyPartTwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyPartTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyPartTwoApplication.class, args);
+	}
+
+}
